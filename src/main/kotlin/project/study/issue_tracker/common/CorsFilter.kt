c250d75bc603f5cd5d84ba08606d5b35caf1b9f0
Raymond <73725736+raymondanythings@users.noreply.github.com>
2023-05-25 16:03:56 +0900

    cors

---
+++ src/main/kotlin/project/study/issue_tracker/common/CorsFilter.kt
@@ -0,0 +1,15 @@
+package project.study.issue_tracker.common
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+
+@Configuration
+class WebConfig : WebMvcConfigurer {
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**")
+            .allowedOriginPatterns("*")
+            .allowCredentials(true)
+    }
+}
